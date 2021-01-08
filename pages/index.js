c3c10888e5b8c0c1ca6a6426368f227a38d19d24
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2021-01-08 11:18:20 +0300

    implements routing

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <h1>Hello World!</h1>
-    </div>
-  )
+function Home() {
+    return <h1>Home Page</h1>
 }
+
+export default Home;
