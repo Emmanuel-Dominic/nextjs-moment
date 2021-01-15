ce4bc8745f47f26cb55fc0d7fbed668a458ffc4f
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2021-01-15 19:23:16 +0300

    implements navigating programmatically

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
+import { useRouter } from "next/router";
+
 function Home() {
-    return <h1>Home Page</h1>
+    const router = useRouter();
+
+    const handleClick = () => {
+        router.push('/product');
+        // router.replace('/product');
+    }
+
+    return (
+    <>
+      <h1>Home Page</h1>
+      <button onClick={handleClick}>Place Order</button>
+    </>)
 }
 
 export default Home;
