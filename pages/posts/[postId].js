7501d050e016a020daa3cf35845b257161ee494d
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2021-02-03 08:31:42 +0300

    implements SSG with getStaticPaths fallback true

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -1,4 +1,11 @@
+import { useRouter } from 'next/router'
+
 function Post({post}) {
+    const router = useRouter()
+
+    if (router.isFallback) {
+        return <h1>Loading...</h1>
+    }
     return (
         <div>
             <h2>{post.id} - {post.title}</h2>
@@ -10,17 +17,19 @@ function Post({post}) {
 export default Post;
 
 export async function getStaticPaths() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const data = await response.json();
-
-    const paths = data.map(post => {
-        return {
-            params: { postId: `${post.id}` }
-        }
-    })
     return {
-        paths,
-        fallback: false
+        paths: [
+            {
+                params: {postId: '1'}
+            },
+            {
+                params: {postId: '2'}
+            },
+            {
+                params: {postId: '3'}
+            }
+        ],
+        fallback: true
     }
 }
 
@@ -28,6 +37,12 @@ export async function getStaticProps(context) {
     const { params } = context;
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`);
     const data = await response.json();
+
+    if (!data.id) {
+        return {
+            notFound: true
+        }
+    }
 
     return {
         props: {
