9c62c08a975536e1b280ba673bee67417e3d1b09
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2021-02-14 12:54:35 +0300

    implements client side data fetching

---
+++ pages/dashboard.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+export default function Dashboard() {
+    const [ isLoading, setIsLoading ] = useState(true)
+    const [ dashboardData, setDashboardData ] = useState(null)
+
+    useEffect(() => {
+        async function fetchDashboardData() {
+            const response = await fetch('http://localhost:4000/dashboard')
+            const data = await response.json()
+            setDashboardData(data)
+            setIsLoading(false)
+        }
+        fetchDashboardData()
+    }, [])
+
+    if (isLoading) {
+        return <h2>Loading...</h2>
+    }
+
+    return (
+        <div>
+            <h2>Dashboard</h2>
+            <h4>Posts - {dashboardData.posts}</h4>
+            <h4>Likes - {dashboardData.likes}</h4>
+            <h4>Followers - {dashboardData.followers}</h4>
+            <h4>Following - {dashboardData.following}</h4>
+        </div>
+    )
+}
