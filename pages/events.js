9b1a0672d507070671e0c823b7d2d1a93b46f14b
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2021-02-17 10:11:16 +0300

    implements pre-rendering with Client-side data fetching

---
+++ pages/events.js
@@ -0,0 +1,46 @@
+import { useState } from "react"
+import { useRouter } from "next/router";
+
+export default function EvenList({eventList}) {
+    const [events, setEvents] = useState(eventList)
+    const router = useRouter()
+
+    const fetchTechnologyEvents = async () => {
+        const response = await fetch(`http://localhost:4000/events?category=technology`);
+        const data = await response.json();
+        setEvents(data)
+        router.push('/events?category=technology', undefined, {shallow: true})
+    }
+    return (
+        <>
+            <button onClick={fetchTechnologyEvents}>Tech Events</button>
+            <h1>List of Events</h1>
+            {
+                events.map(event => {
+                    return (
+                        <div key={event.id}>
+                            <h3>{event.id} . {event.title} - {event.date} | {event.category}</h3>
+                            <p>{event.description}</p>
+                            <hr />
+                        </div>
+                    )
+                })
+            }
+        </>
+    )
+}
+
+
+export async function getServerSideProps(context) {
+    const { query } = context
+    const { category } = query
+    const queryString = category ? 'category=technology' : ""
+    const response = await fetch(`http://localhost:4000/events?${queryString}`)
+    const data = await response.json()
+
+    return {
+        props: {
+            eventList: data
+        }
+    }
+}
