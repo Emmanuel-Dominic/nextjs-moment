e7ff0fa1fe059de576b66325f58651a7ed5cf53e
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2021-03-09 15:15:20 +0300

    implements global styling

---
+++ pages/contact.js
@@ -0,0 +1,8 @@
+export default function Contact() {
+    return (
+        <>
+            <h2>Contact Page</h2>
+            <button className="btn btn-lg btn-primary">Submit</button>
+        </>
+    )
+}
