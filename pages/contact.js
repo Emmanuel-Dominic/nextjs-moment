d4ef04d3332c2ee412c450aff3093d63addc9519
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2021-03-10 21:10:52 +0300

    implements component level styling

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,10 @@
+import styles from '../styles/Contact.module.css'
+
 export default function Contact() {
     return (
         <>
             <h2>Contact Page</h2>
+            <p className={styles.highlight}>ematembu2@gmail.com</p>
             <button className="btn btn-lg btn-primary">Submit</button>
         </>
     )
