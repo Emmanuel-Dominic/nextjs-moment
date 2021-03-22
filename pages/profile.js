8a609dd4a2cf4f69568c9db85c08e4bbb2652fa0
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2021-03-22 11:23:11 +0300

    implements image component

--- pages/profile.js
+++ pages/profile.js
@@ -1,5 +1,18 @@
+import Image from 'next/image'
+
 function Profile() {
-    return <h1>Profile Page</h1>
+    return (<>
+        <h1>Profile Page</h1>
+        {
+            ['1', '2'].map(path => {
+                return (
+                    <div key={path}>
+                        <Image src={`/user${path}.jpg`} alt={path} width='280' height='420' />
+                    </div>
+                )
+            })
+        }
+    </>)
 }
 
 export default Profile;
