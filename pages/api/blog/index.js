5b83b281e8b9abb7964699474707bc29ee47c659
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2021-04-04 11:21:27 +0300

    implements securing api routes

--- pages/api/blog/index.js
+++ pages/api/blog/index.js
@@ -1,3 +1,10 @@
-export default function handler(req, res) {
-    res.status(200).json({name: "Blog API route"})        
+import { getSession } from 'next-auth/client'
+
+export default async function handler(req, res) {
+    const session = await getSession({req})
+    if (!session) {
+        res.status(401).json({error: 'unauthenticated user'})
+    } else {
+        res.status(200).json({name: "Blog API route", session})
+    }
 }
